3cb60f7359c6369f2b45e862ed90d9c923bcc9f0
AndyRdn <153283134+AndyRdn@users.noreply.github.com>
2024-12-20 13:48:21 +0300

    initializer

---
+++ src/test/java/org/main/criptoapi/CriptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.main.criptoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
