713f09eacfd451dda672e8a6881de4e4f9506942
AndyRdn <153283134+AndyRdn@users.noreply.github.com>
2025-01-10 12:26:11 +0300

    test

--- src/main/java/org/main/criptoapi/mock/SidebarMock.java
+++ src/main/java/org/main/criptoapi/mock/SidebarMock.java
@@ -7,24 +7,18 @@ public class SidebarMock {
     public static final Sidebar DEFAULT_SIDEBAR = new Sidebar("bx bx-chair", "Mr Meuble") // logo + nom du projet
         .withLien("/home") // Lien lorsqu'on clique sur le logo
         .addMenu(
-            // Menu: Besoin
-            new Menu("Besoins")
-                .withIcon("bx bx-briefcase-alt") // icon du menu: a voir sur: boxicons.com -> clique sur
-                // icon -> font -> copier la class
-                .addSubmenu(
-                    new Menu("Demande de besoin") // Besoin (menu) -> Demande de Besoin (sous-menu du
-                        // menu besoin)
-                        .withIcon("bx bx-mail-send")
-                        .withLien("/besoin/form")
-                )
-                .addSubmenu(
-                    new Menu("Validation Besoin") // Besoin -> Validation Besoin
-                        .withIcon("bx bx-task")
-                        .withLien("/besoin/list")
-                )
+                // Menu: Besoin
+                new Menu("Dashboard")
+                        .withIcon("bx bx-briefcase-alt") // icon du menu: a voir sur: boxicons.com -> clique sur
+                        // icon -> font -> copier la class
+                        .addSubmenu(
+                                new Menu("Acceuil") // Besoin (menu) -> Demande de Besoin (sous-menu du
+                                        // menu besoin)
+                                        .withIcon("bx bx-mail-send")
+                                        .withLien("/dashborad/")
+                        )
 
-        )
-        .addMenu(
+        ).addMenu(
                 // Menu: Besoin
                 new Menu("Profil")
                         .withIcon("bx bx-briefcase-alt") // icon du menu: a voir sur: boxicons.com -> clique sur
