f8e1844c0b5628cc2e1cbfef4c2c91b70161127e
Miarantsoa Ainaharison <miarantsoaainaharison@gmail.com>
2025-01-22 07:02:42 +0300

    docker

--- src/main/resources/sql/crypto_api.sql
+++ src/main/resources/sql/crypto_api.sql
@@ -1,3 +1,61 @@
+CREATE TABLE fonds (
+    id SERIAL PRIMARY KEY ,
+    idUser INT,
+    depot numeric(15,2),
+    retrait numeric(15,2),
+    daty timestamp
+);
+
+CREATE TABLE Crypto(
+    id serial primary key ,
+    nom varchar(256),
+    daty timestamp
+);
+
+CREATE TABLE ListCrypto(
+    id serial primary key ,
+    id_crypto int references Crypto(id),
+    daty timestamp
+);
+
+CREATE TABLE mtv_crypto(
+    id serial primary key ,
+    id_crypto int references Crypto(id),
+    id_user int,
+    achat int,
+    vente int,
+    daty timestamp,
+    valeur numeric(15,2)
+);
+
+CREATE OR REPLACE VIEW v_solde_cryptos AS
+(
+    SELECT id_user, id_crypto, SUM(achat) - SUM(vente) AS quantite FROM mtv_crypto GROUP BY id_user, id_crypto ORDER BY id_user
+);
+
+create table histo_Crypto(
+                             id serial primary key ,
+                             id_Crypto int,
+                             daty timestamp,
+                             valeur decimal(15,2)
+);
+
+insert into histo_Crypto values (default, 1, '2024-1-1 10:00:00', 2200.40);
+insert into histo_Crypto values (default, 2, '2024-1-1 10:00:00', 5000.99);
+insert into histo_Crypto values (default, 3, '2024-1-1 10:00:00', 1500.50);
+insert into histo_Crypto values (default, 4, '2024-1-1 10:00:00', 1800.60);
+insert into histo_Crypto values (default, 5, '2024-1-1 10:00:00', 3200.75);
+insert into histo_Crypto values (default, 6, '2024-1-1 10:00:00', 4100.80);
+insert into histo_Crypto values (default, 7, '2024-1-1 10:00:00', 750.30);
+insert into histo_Crypto values (default, 8, '2024-1-1 10:00:00', 1200.35);
+insert into histo_Crypto values (default, 9, '2024-1-1 10:00:00', 300.25);
+insert into histo_Crypto values (default, 10, '2024-1-1 10:00:00', 6500.99);
+
+CREATE OR REPLACE VIEW v_solde_user AS
+(
+    SELECT iduser, SUM(depot) - SUM(retrait) AS solde FROM fonds GROUP BY iduser
+);
+
 
 INSERT INTO Crypto (nom, daty) VALUES ('Bitcoin', '2023-01-01 10:00:00');
 INSERT INTO Crypto (nom, daty) VALUES ('Ethereum', '2023-02-15 11:30:00');
