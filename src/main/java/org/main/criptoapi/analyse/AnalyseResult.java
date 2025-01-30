4e665ed94da3af1167cbee0b7a249a5d3c58b2a1
Tsinjoniaina18 <tsinjoniaina.randriamirado@gmail.com>
2025-01-30 09:36:27 +0300

    Update

--- src/main/java/org/main/criptoapi/analyse/AnalyseResult.java
+++ src/main/java/org/main/criptoapi/analyse/AnalyseResult.java
@@ -3,6 +3,8 @@ package org.main.criptoapi.analyse;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class AnalyseResult {
@@ -12,5 +14,11 @@ public class AnalyseResult {
     private double max;
     private double ecartType;
 
-    public 
+    public AnalyseResult(Integer id, BigDecimal quartile, BigDecimal min, BigDecimal max, BigDecimal ecart){
+        this.id = id;
+        this.quartile = quartile.doubleValue();
+        this.min = min.doubleValue();
+        this.max = max.doubleValue();
+        this.ecartType = ecart.doubleValue();
+    }
 }
